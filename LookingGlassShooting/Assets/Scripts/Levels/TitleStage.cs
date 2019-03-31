ab70fda933a4bcef7af5f8167ca3847a0da9ddb3
m2wasabi <m2wasabi@gmail.com>
2019-03-31 11:29:23 +0900

    Title画面のシナリオ

---
+++ LookingGlassShooting/Assets/Scripts/Levels/TitleStage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleStage : MonoBehaviour
+{
+    private SceneController _sceneController;
+    private bool _inTransition = false;
+    void Start()
+    {
+        _sceneController = GetComponent<SceneController>();
+    }
+
+    void Update()
+    {
+        if (Input.anyKey && !_inTransition)
+        {
+            _inTransition = true;
+            GoCharacterSelect();
+        }
+    }
+
+    private void GoCharacterSelect()
+    {
+        if (_sceneController != null)
+        {
+            _sceneController.ShowCharacterSelect();
+        }
+    }
+}
