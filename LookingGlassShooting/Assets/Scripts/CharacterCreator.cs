aebf79a52b1e3045215e079b5089b3c560d14879
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2019-03-31 14:57:55 +0900

    キャラクター生成実装

---
+++ LookingGlassShooting/Assets/Scripts/CharacterCreator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CharacterCreator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] m_characters;
+
+    private void Start()
+    {
+        var n = Random.Range(0, m_characters.Length);
+
+        var rotationY = 0;
+        if (gameObject.tag == "Player2")
+            rotationY = 180;
+
+        Instantiate(m_characters[n],
+            transform.position,
+            Quaternion.Euler(new Vector3(0, rotationY, 0)),
+            transform);
+    }
+}
